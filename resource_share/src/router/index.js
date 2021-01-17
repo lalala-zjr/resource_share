2c526ade55320be1a6611ee36cea19f4fe862460
lalala-zjr <1365351516@qq.com>
2021-01-17 22:26:24 +0800

    主页

--- resource_share/src/router/index.js
+++ resource_share/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import register from '@/pages/register'
+import main from '@/pages/main'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'register',
       component: register
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: main
     }
   ]
 })
