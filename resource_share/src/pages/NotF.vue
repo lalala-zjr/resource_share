708ba6b332e2ce06d997f3317bad1b69f3468a0b
lalala-zjr <1365351516@qq.com>
2021-01-17 22:46:05 +0800

    NotFound

---
+++ resource_share/src/pages/NotF.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <img src="../assets/404.jpg" alt="哎呀图片不见了">
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+img{
+    width: 100%;
+    height: 540px;
+}
+</style>
