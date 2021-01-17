3a1e69cfb4e7c722730f8e0aec8ffaa2699c8805
lalala-zjr <1365351516@qq.com>
2021-01-17 22:34:18 +0800

    404

--- resource_share/src/router/index.js
+++ resource_share/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import register from '@/pages/register'
 import main from '@/pages/main'
+import NotF from '@/pages/NotF'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: main
+    },
+    {
+      path: '*',
+      name: 'NotF',
+      component: NotF
     }
   ]
 })
