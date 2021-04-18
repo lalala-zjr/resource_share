71ba632c68455bd98c9aad2cd612987702f7b8f8
zjr-lalala <1365351516@qq.com>
2021-04-18 22:51:59 +0800

    echarts

--- resource_share/src/main.js
+++ resource_share/src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 import qs from 'qs'
+import echarts from 'echarts'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
 Vue.prototype.qs = qs
+Vue.prototype.echarts = echarts
 
 /* eslint-disable no-new */
 new Vue({
